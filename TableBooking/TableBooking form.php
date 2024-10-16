6b3816f8add89a0628fccb4dd35b6ed951f7a5ac
NickGrahovskis <nick.grahovskis@student.nhlstenden.com>
2024-10-16 12:50:12 +0200

    Added Time check functionv and several changes

--- TableBooking/TableBooking form.php
+++ TableBooking/TableBooking form.php
@@ -4,34 +4,76 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="TableBooking form.css" rel="stylesheet" type="text/css">
-    
+    <link href="./TableBooking form.css" rel="stylesheet" type="text/css">
 </head>
+ <?php
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    $fname = filter_input(INPUT_POST, "fname");
+    $email = filter_input(INPUT_POST, "email");
+    $phoneNr = filter_input(INPUT_POST, "phoneNr", FILTER_SANITIZE_NUMBER_INT);
+    $bookDate = filter_input(INPUT_POST, "bookDate", FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
+    $time = filter_input(INPUT_POST, "time");
+    $nrGuests = filter_input(INPUT_POST, "nrGuests", FILTER_SANITIZE_NUMBER_INT);
+
+    
+
+    function  checkTime($vremya){
+        if(is_numeric($vremya[0]) && is_numeric($vremya[3]) && $vremya[1] == ":" ){
+           echo"dasdadaa";
+        }else{
+            doubleCheckTime($vremya);
+        }
+    }
+    function doubleCheckTime($vremya){
+        if(is_numeric($vremya[0])&& is_numeric($vremya[1]) && $vremya[2] == ":" && is_numeric($vremya[3])){
+            echo"good to go";
+        }else{
+            echo '<h2 class="alert" >wrong time</h2>';
+        }
+    }
+
+    if(empty($time) ){
+        echo"Input is empty";
+    }
+    else{
+        checkTime($time);
+    }
+
+}
+    
+
+
+
+?>
+<?php
+    var_dump($_POST);
+?> 
 <body background="Project/Bg.webp" class="bgImage">
 <main>
-    <div class="Table1">
+    <div class="table1">
         <h2>Table booking form</h2>
-        <div class="TableForm">
-            <form action=" "<?php  echo $_SERVER["PHP_SELF"]?>  method="post">
-                <label for="Fname">Name</label>
-                <input type="text" id ="Fname" >
-                <label for="Email">Email</label>
-                <input type="email" id ="Email">
-                <label for="PhoneNr">Phone number</label>
-                <input type="number" id ="PhoneNr" >
-                <div class="TimeDiv">
-                    <label for="BookDate" class="Book">Booking date</label>
-                    <label for="Time" class="Time-Label">Time</label>
+        <div class="tableForm">
+            <form action="<?php echo $_SERVER["PHP_SELF"]?>"  method="POST">
+                <label for="fname">Name</label>
+                <input type="text" id ="fname" name="fname" >
+                <label for="email">Email</label>
+                <input type="email" id ="email" name="fname">
+                <label for="phoneNr">Phone number</label>
+                <input type="number" id ="phoneNr" name="phoneNr">
+                <div class="timeDiv">
+                    <label for="bookDate" class="book">Booking date</label>
+                    <label for="time" class="time-Label">Time</label>
                 </div>
-                <div class="Input-Div">
-                    <input type="date" id ="BookDate" >
-                    <span><input type="datetime" id ="Time" ></span>
+                <div class="input-Div">
+                    <input type="date" id ="bookDate" >
+                    <input type="text" id ="time" name="time"> 
                 </div>
-                <label for="NrGuests">Number of guests</label>
-                <input type="number" id ="NrGuests" >
+                <label for="nrGuests">Number of guests</label>
+                <input type="number" id ="nrGuests" name="nrGuests" >
                 
                
-                <button class="TableBookingButton">
+                <button class="tablebook-button">
                 <h3>Reserve a table</h3>
                 </button>
                 
@@ -44,41 +86,7 @@
     </div>
 </main>  
 
-<?php
-if($_SERVER["REQUEST_METHOD"] == "POST")
-{
-    $fname = filter_input(INPUT_POST, "fname");
-    $lname = filter_input(INPUT_POST, "lname");
-    $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
-    $sports = filter_input(INPUT_POST, "sport", FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
-    $communication = filter_input(INPUT_POST, "communication");
-    $sub = filter_input(INPUT_POST, "sub");
-}
 
-
-function CheckEmail(){
-    $EmailExample = "";
-    if($EmailExample != "@" || $EmailExample != ".com"){
-        echo "Your email does not contain";
-    }
-    else{
-        echo"";
-    }
-
-}
-
-function CheckTime(){
-    $Time = "";
-    if(ctype_alpha($Time)){
-        echo "Time contain letters";
-    }
-    else{
-        echo"";
-    }
-}
-CheckTime();
-
-?>
 
 </body>
 </html>
