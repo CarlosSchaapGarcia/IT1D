fb9ea697e4eb16e9128e236d3bd48af1ce3f8ec3
CarlosSchaapGarcia <carlos.schaap.garcia@student.nhlstenden.com>
2024-10-18 09:50:41 +0200

    Changed the name of the folder and added bg

---
+++ OuiWebsite/main.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oui</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body background="img/Bg.webp" class="bgImage">
+    <div class="main">
+        <div class="navMenu">
+            <ul>
+                <li><a><img src="Project/Logo-02_2.png" class="Logo"></a></li>
+                <li class="buttons"><a href="" >Home</a></li>
+                <li class="buttons"><a href="" >About us</a></li>
+                <li class="buttons"><a href="" >Menu</a></li>
+                <li class="buttons"> <a href="" >Events</a></li>
+                <li class="buttons"><a href="" >Reviews</a></li>
+                <li class="buttons"><a href="" >Feedback</a></li>
+                <div class="squareforBooking"><li class="button-book"><a href="" class="customLetters">Book a table</a></li></div>
+            </ul>
+        </div>
+    </div>
+    
+</body>
+</html>
