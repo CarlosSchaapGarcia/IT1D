bdac9925ec2faad8f2c0e73ee9c4cc05e7020fbb
DavidCorodeanu <david.corodeanu@student.nhlstenden.com>
2024-10-18 10:26:07 +0200

    Added the about us section of the page with some css changes related to it and also added the circular and caveat font faces

--- OuiWebsite/main.php
+++ OuiWebsite/main.php
@@ -21,6 +21,60 @@
             </ul>
         </div>
     </div>
+    <aboutus>
+        <div class="whyarewedifferent">Why are we different?</div>
+        <div class="whydifferent">
+            <div class="whybox">
+                <img class="coffeebeans" src="./img/coffee-beans_1-removebg-preview.png" alt="coffee beans">
+                <div class="supremebeans">Supreme Beans</div>
+                <div class="beansgreattaste">Beans that provides<br>great taste</div>
+            </div>
+            <div class="whybox">
+                <img class="coffeebeans" src="./img/badge_1-removebg-preview.png" alt="badge">
+                <div class="supremebeans">High Quality</div>
+                <div class="beansgreattaste">We provide the<br>highest quality</div>
+            </div>
+            <div class="whybox">
+                <img class="coffeebeans" src="./img/coffee-cup_1-removebg-preview.png" alt="coffee cup">
+                <div class="supremebeans"> Extraordinary</div>
+                <div class="beansgreattaste">Coffee like you have<br>never tasted</div>
+            </div>
+            <div class="whybox">
+                <img class="coffeebeans" src="./img/best-price_1-removebg-preview.png" alt="best price">
+                <div class="supremebeans">Affordable Price</div> 
+                <div class="beansgreattaste">Our Coffee prices are<br>easy to afford</div>
+            </div>
+        </div>
+        <div class="aboutus1">
+            <div class="aboutus">About us</div>
+            <div class="aboutustxt">Welcome to our café, where Paris meets<br> the Netherlands. </div>
+            <div class="aboutustxt">We were inspired to start a French-inspired coffee<br> shop in the Netherlands after experiencing the<br> charming and elegant coffee shops in Paris
+                 during the<br> Olympic Games. The ambiance, the aroma of freshly<br> brewed coffee, and the delightful pastries created an<br> unforgettable experience that
+                  we want to bring to our<br> community. </div>
+            <div class="aboutustxt">Come by and let us bring a little bit of French magic to<br> your day!</div>
+        </div>
+    </aboutus>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 </body>
 </html>
